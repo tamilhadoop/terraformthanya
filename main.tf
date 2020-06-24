6922dc4ffb66a5a89abdcefe9e5d4b41cf8a45ec
selvam <me.selvam@gmail.com>
2020-06-24 09:26:55 +0100

    test

---
+++ main.tf
@@ -0,0 +1,70 @@
+provider "azurerm" {
+    version = "~> 2.15"
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "${var.location}"
+}
+
+resource "azurerm_cosmosdb_account" "example" {
+  name                = "${var.prefix}-cosmosdb"
+  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    prefix            = "${var.prefix}-customid"
+    location          = "${azurerm_resource_group.example.location}"
+    failover_priority = 0
+  }
+}
+
+##Key vault
+data "azurerm_client_config" "current" {}
+resource "azurerm_key_vault" "example" {
+  name                        = "testvault"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_enabled         = true
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "get",
+    ]
+
+    secret_permissions = [
+      "get",
+    ]
+
+    storage_permissions = [
+      "get",
+    ]
+  }
+
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+  }
+
+  tags = {
+    environment = "Testing"
+  }
+}
+
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+location = "eastus"
+prefix = "tf"
